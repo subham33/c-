e1291ee03459adddbc27771cf472c3cae2d1fc40
subham ashutosh <subhamashutosh33@gmail.com>
2020-10-25 13:21:44 +0530

    second commit

---
+++ pattern2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=a-i;j++)
+        {
+            cout<<"  ";
+        }
+        for(int j=1;j<=a;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
